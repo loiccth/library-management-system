0c1c63d2086d3c59f710d8c90020d9ecaf8bb893
Loïc Cth <50406142+loiccth@users.noreply.github.com>
2020-11-01 17:29:02 +0400

    Added librarian methods

--- backend/models/users/librarian.model.js
+++ backend/models/users/librarian.model.js
@@ -1,9 +1,110 @@
 const mongoose = require('mongoose')
 const User = require('./user.base')
+const Book = require('../book.model')
+const csv = require('csv-parser')
+const fs = require('fs');
+
 
 const Schema = mongoose.Schema
 
 const librarianSchema = new Schema()
+
+librarianSchema.methods.addBook = function (book, res) {
+    const { title, author, isbn, publisher, publishedDate, edition, category, description, noOfPages, location, campus, purchasedOn } = book
+
+    if (title === null || author === null || isbn === null || publisher === null || publishedDate === null || edition === null || category === null
+        || description === null || noOfPages === null || location === null || campus === null || purchasedOn === null) return res.json({ 'error': 'Missing params' })
+    else {
+        Book.findOne({ isbn })
+            .then(book => {
+                if (book === null) {
+                    const newBook = new Book({
+                        title,
+                        author,
+                        isbn,
+                        publisher,
+                        publishedDate,
+                        edition,
+                        category,
+                        description,
+                        noOfPages,
+                        location,
+                        campus,
+                        copies: {
+                            purchasedOn
+                        }
+                    })
+                    newBook.save()
+                        .then(() => res.sendStatus(201))
+                        .catch(err => { throw err })
+                }
+                else {
+                    book.copies.push({
+                        purchasedOn
+                    })
+                    book.save()
+                        .then(() => res.sendStatus(201))
+                        .catch(err => { throw err })
+                }
+            })
+            .catch(err => { throw err })
+    }
+}
+
+librarianSchema.methods.addBookCSV = function (file, res) {
+    let success = []
+    let fail = []
+
+    const stream = fs.createReadStream(file)
+        .pipe(csv())
+        .on('data', async (book) => {
+            stream.pause()
+            const { title, author, isbn, publisher, publishedDate, edition, category, description, noOfPages, location, campus, purchasedOn } = book
+
+            await Book.findOne({ isbn })
+                .then(async (book) => {
+                    if (book === null) {
+                        const newBook = new Book({
+                            title,
+                            author,
+                            isbn,
+                            publisher,
+                            publishedDate,
+                            edition,
+                            category,
+                            description,
+                            noOfPages,
+                            location,
+                            campus,
+                            copies: {
+                                purchasedOn
+                            }
+                        })
+                        newBook.save()
+                            .then(() => success.push(title))
+                            .catch((err) => fail.push(title + ' - ' + err.message))
+                    }
+                    else {
+                        book.copies.push({
+                            purchasedOn
+                        })
+                        await book.save()
+                            .then(() => success.push(title))
+                            .catch((err) => fail.push(title + ' - ' + err.message))
+                    }
+                })
+                .catch(err => { throw err })
+            stream.resume()
+        })
+        .on('end', () => {
+            setTimeout(() => {
+                res.json({
+                    success,
+                    fail
+                })
+            }, 500);
+        })
+}
 
 const Librarian = User.discriminator('Librarian', librarianSchema)
 
--- backend/routes/books.js
+++ backend/routes/books.js
@@ -3,8 +3,6 @@ const router = express.Router();
 const jwt = require('express-jwt');
 const multer = require('multer')
 const upload = multer({ dest: 'uploads/books/' })
-const csv = require('csv-parser')
-const fs = require('fs');
 const Book = require('../models/book.model')
 const Borrow = require('../models/transactions/borrow.model')
 const Reserve = require('../models/transactions/reserve.model')
@@ -12,118 +10,29 @@ const Transaction = require('../models/transactions/transaction.base');
 const Member = require('../models/users/member.model');
 const MemberA = require('../models/users/member_accademic.model');
 const MemberNA = require('../models/users/member_non_accademic.model');
+const Librarian = require('../models/users/librarian.model');
 const secret = process.env.JWT_SECRET;
 
 // Add a single book
 router.post('/add_single', jwt({ secret, credentialsRequired: true, getToken: (req) => { return req.cookies.jwttoken }, algorithms: ['HS256'] }), (req, res) => {
-    // if (req.user.memberType !== 'Librarian') return res.sendStatus(403)
-
-    const { title, author, isbn, publisher, publishedDate, edition, category, description, noOfPages, location, campus, purchasedOn } = req.body
-
-    Book.findOne({ isbn })
-        .then(book => {
-            if (book === null) {
-                const newBook = new Book({
-                    title,
-                    author,
-                    isbn,
-                    publisher,
-                    publishedDate,
-                    edition,
-                    category,
-                    description,
-                    noOfPages,
-                    location,
-                    campus,
-                    copies: {
-                        purchasedOn
-                    }
-                })
-                newBook.save()
-                    .then(() => res.sendStatus(201))
-                    .catch(err => res.status(400).json({
-                        'success': false,
-                        'error': err.message
-                    }))
-            }
-            else {
-                book.copies.push({
-                    purchasedOn
-                })
-                book.save()
-                    .then(() => res.sendStatus(200))
-                    .catch(err => res.status(400).json({
-                        'success': false,
-                        'error': err.message
-                    }))
-            }
-        })
-        .catch(err => res.status(400).json({
-            'success': false,
-            'error': err.message
-        }))
+    if (req.user.memberType !== 'Librarian') return res.sendStatus(403)
+    else {
+        Librarian.findOne({ _id: req.user._id })
+            .then(librarian => {
+                librarian.addBook(req.body, res)
+            })
+    }
 })
 
 // Add multiple book from csv file
 router.post('/add', jwt({ secret, credentialsRequired: true, getToken: (req) => { return req.cookies.jwttoken }, algorithms: ['HS256'] }), upload.single('csv'), (req, res) => {
-    // if (req.user.memberType !== 'Librarian') return res.sendStatus(403)
-
-    let success = []
-    let fail = []
-
-    const stream = fs.createReadStream(req.file.path)
-        .pipe(csv())
-        .on('data', async (book) => {
-            stream.pause()
-            const { title, author, isbn, publisher, publishedDate, edition, category, description, noOfPages, location, campus, purchasedOn } = book
-
-            await Book.findOne({ isbn })
-                .then(async (book) => {
-                    if (book === null) {
-                        const newBook = new Book({
-                            title,
-                            author,
-                            isbn,
-                            publisher,
-                            publishedDate,
-                            edition,
-                            category,
-                            description,
-                            noOfPages,
-                            location,
-                            campus,
-                            copies: {
-                                purchasedOn
-                            }
-                        })
-                        newBook.save()
-                            .then(() => success.push(title))
-                            .catch(() => fail.push(title))
-                    }
-                    else {
-                        book.copies.push({
-                            purchasedOn
-                        })
-                        await book.save()
-                            .then(() => success.push(title))
-                            .catch(() => fail.push(title))
-                    }
-                })
-                .catch(err => {
-                    return res.status(400).json({
-                        'error': err.message
-                    })
-                })
-            stream.resume()
-        })
-        .on('end', () => {
-            setTimeout(() => {
-                res.json({
-                    success,
-                    fail
-                })
-            }, 500);
-        })
+    if (req.user.memberType !== 'Librarian') return res.sendStatus(403)
+    else {
+        Librarian.findOne({ _id: req.user._id })
+            .then(librarian => {
+                librarian.addBookCSV(req.file.path, res)
+            })
+    }
 })
 
 // Borrow a book
